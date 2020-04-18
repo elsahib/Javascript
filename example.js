66fb58521820b233330336e98d591e6d0d45c621
elsahib10@gmail.com <elsahib10@gmail.com>
2020-04-18 17:09:41 +0100

    first commit

---
+++ example.js
@@ -0,0 +1,27 @@
+// //excercise 3
+// const greeting = "Hello"; // First variable
+// const name = "Osman"; // Second Variable
+
+// console.log(greeting + ' ' + name); // first way to concatinate
+// console.log(greeting.concat(' ', name)); // using concat function to concatinate 
+
+
+// // excercise 4
+// const numberOfStudents = 20;
+// const NumberOfMentors = 10;
+
+// console.log(numberOfStudents+NumberOfMentors)
+
+
+
+
+
+
+// Excercise 5
+
+function test(fname,lname,age) {
+
+    return "Hello"+" "+ fname+" "+lname+": in 10 years you will be "+(age+10)
+
+}
+console.log(test('osman','elsahib',24))
